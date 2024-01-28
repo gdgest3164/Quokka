30d350f77b9124df7dda8b3e3198e54bf5c6d042
gdgest3164 <rlaehdgs12@naver.com>
2024-01-28 19:39:17 +0900

    fix. 쿠키 저장

--- app/Components/Layouts/sidebar.tsx
+++ app/Components/Layouts/sidebar.tsx
@@ -55,7 +55,7 @@ export default function Sidebar(brand: SidebarProps) {
                 {/* <span className="inline-flex items-center justify-center px-2 ms-3 text-sm font-medium text-gray-800 bg-gray-100 rounded-full dark:bg-gray-700 dark:text-gray-300">Pro</span> */}
               </Link>
             </li>
-            <li>
+            {/* <li>
               <Link to="/" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group" prefetch="intent">
                 <svg
                   className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
@@ -97,7 +97,7 @@ export default function Sidebar(brand: SidebarProps) {
                 </svg>
                 <span className="flex-1 ms-3 whitespace-nowrap">Products</span>
               </Link>
-            </li>
+            </li> */}
           </ul>
         </div>
       </aside>
--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,4 @@
-import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration, useLoaderData } from "@remix-run/react";
+import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration, json, useLoaderData } from "@remix-run/react";
 import type { LoaderFunction, MetaFunction } from "@remix-run/node";
 import clsx from "clsx";
 
@@ -7,6 +7,7 @@ import { getThemeSession } from "./utils/theme.server";
 
 import styles from "./tailwind.css";
 import Sidebar, { SidebarProps } from "./Components/Layouts/sidebar";
+import { commitSession, getSession } from "./cookies.server";
 
 export const meta: MetaFunction = () => {
   const title = "쿼카 재고관리";
@@ -30,23 +31,39 @@ export type LoaderData = {
 };
 
 export const loader: LoaderFunction = async ({ request }) => {
-  const themeSession = await getThemeSession(request);
+  const session = await getSession(request.headers.get("Cookie"));
+  const brand_info = session.get("brand_info");
 
-  const response = await fetch(`http://quokka.run:8000/api/seller/brand?channelNo=${"100987434"}`, {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
+  if (!brand_info) {
+    const themeSession = await getThemeSession(request);
+    const response = await fetch(`http://quokka.run:8000/api/seller/brand?channelNo=${"100987434"}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const responseData = await response.json();
+    const data: LoaderData = {
+      theme: themeSession.getTheme(),
+      brand: responseData,
+    };
+    session.set("brand_info", responseData);
+    const cookie = await commitSession(session, { domain: "localhost", path: "/" });
 
-  const responseData = await response.json();
+    json(
+      { brand_info },
+      {
+        headers: {
+          "Set-Cookie": cookie,
+        },
+      }
+    );
 
-  const data: LoaderData = {
-    theme: themeSession.getTheme(),
-    brand: responseData,
-  };
-
-  return data;
+    // document.cookie = `brand=${JSON.stringify(data.brand)}`;
+    return data;
+  } else {
+    return brand_info;
+  }
 };
 
 function App() {
