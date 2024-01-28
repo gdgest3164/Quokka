4bd68d4df446ff4e10f2d050de3756e8b7b68797
gdgest3164 <rlaehdgs12@naver.com>
2024-01-28 20:28:08 +0900

    fix. 쿠키 보류

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,4 @@
-import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration, json, useLoaderData } from "@remix-run/react";
+import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration, useLoaderData } from "@remix-run/react";
 import type { LoaderFunction, MetaFunction } from "@remix-run/node";
 import clsx from "clsx";
 
@@ -7,7 +7,6 @@ import { getThemeSession } from "./utils/theme.server";
 
 import styles from "./tailwind.css";
 import Sidebar, { SidebarProps } from "./Components/Layouts/sidebar";
-import { commitSession, getSession } from "./cookies.server";
 
 export const meta: MetaFunction = () => {
   const title = "쿼카 재고관리";
@@ -31,44 +30,46 @@ export type LoaderData = {
 };
 
 export const loader: LoaderFunction = async ({ request }) => {
-  const session = await getSession(request.headers.get("Cookie"));
-  const brand_info = session.get("brand_info");
+  // const session = await getSession(request.headers.get("Cookie"));
+  // const brand_info = session.get("brand_info");
 
-  if (!brand_info) {
-    const themeSession = await getThemeSession(request);
-    const response = await fetch(`http://quokka.run:8000/api/seller/brand?channelNo=${"100987434"}`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    const responseData = await response.json();
-    const data: LoaderData = {
-      theme: themeSession.getTheme(),
-      brand: responseData,
-    };
+  // if (!brand_info) {
+  const themeSession = await getThemeSession(request);
+  const response = await fetch(`http://quokka.run:8000/api/seller/brand?channelNo=${"100987434"}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const responseData = await response.json();
+  const data: LoaderData = {
+    theme: themeSession.getTheme(),
+    brand: responseData,
+  };
+  return data;
 
-    session.set("brand_info", data);
-    const cookie = await commitSession(session);
+  // session.set("brand_info", data);
+  // const cookie = await commitSession(session);
 
-    return json(
-      { brand_info },
-      {
-        headers: {
-          "Set-Cookie": await cookie,
-        },
-      }
-    );
+  // return json(
+  //   { brand_info },
+  //   {
+  //     headers: {
+  //       "Set-Cookie": cookie,
+  //     },
+  //   }
+  // );
 
-    // document.cookie = `brand=${JSON.stringify(data.brand)}`;
-    // return data;
-  } else {
-    return json(brand_info);
-  }
+  // document.cookie = `brand=${JSON.stringify(data.brand)}`;
+  // return data;
+  // } else {
+  //   return json(brand_info);
+  // }
 };
 
 function App() {
   const data = useLoaderData<LoaderData>();
+  // const { state } = useNavigation();
   const [theme] = useTheme();
 
   return (
