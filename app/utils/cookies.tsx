09d62ee4c8b9ae252400a8fea353a2fef75465fa
gdgest3164 <rlaehdgs12@naver.com>
2024-02-11 12:37:02 +0900

    fix. 쿠키 설정 재수정

--- app/utils/cookies.tsx
+++ app/utils/cookies.tsx
@@ -26,6 +26,6 @@ export const channelNoCookie = async (name: string, code: string, datas: channel
   const session = await getSession(code);
   const expires = new Date(Date.now() + 1000 * 60 * 60 * 24 * 1); // 하루 후 만료
   session.set(name, datas);
-  const create_cookie = await commitSession(session, { sameSite: "none", expires, httpOnly: true, secure: true });
+  const create_cookie = await commitSession(session, { sameSite: "lax", expires, httpOnly: true, secure: true });
   return create_cookie;
 };
